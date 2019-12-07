5fa7f9a5264d1c3144505456e8e69816bda188c4
schmidmj <mitchellsch@live.com>
2019-12-07 14:42:41 -0500

    cms support for adding events

--- contentManagement/js/index.js
+++ contentManagement/js/index.js
@@ -57,4 +57,29 @@
         return false;
     });
 
+    $('#greetingTextSubmit').on('click', function () {
+        const eventName = $("#newEventName").val();
+        const eventLocation = $("#newEventLocation").val();
+        const eventDescription = $("#newEventDescription").val();
+        const eventDate = $("#newEventDate").val();
+        $.ajax({
+            url: dataBaseURL + "textFields/" + "greetingText",
+            type: 'put',
+            dataType: "JSON",
+            async: false,
+            data: {
+                name: eventName,
+                description: eventDescription,
+                location: eventLocation,
+                date: eventDate
+            },
+            success: (textField) => {
+                console.log("Success");
+            },
+            error: (req, status, err) => {
+                console.log("Failure");
+            }
+        });
+    });
+
 })();
